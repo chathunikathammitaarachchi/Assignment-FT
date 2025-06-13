09b950c8572bb31ea49ea0d4abac0da87323761e
chathunikathammitaarachchi <chathunikathammitaarachchi74@gmail.com>
2025-06-13 21:44:43 +0530

    add

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true },  // Ensure 'required' is set to true
+    name: { type: String, required: true, unique: true },  
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, default: 'user' }
